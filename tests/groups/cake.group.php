9dc534e04c3a5f59db862eb5d647ab752c1cc54c
Masaharu Takishita <info@ecworks.jp>
2012-02-12 01:08:12 +0900

    Version 0.5.1

--- tests/groups/cake.group.php
+++ tests/groups/cake.group.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ * CakeKtaiLibrary, supports Japanese mobile phone sites coding.
+ * This provides many functions such as a carrier check to use Referer or E-mail, 
+ * conversion of an Emoji, and more.
+ *
+ * PHP versions 5
+ *
+ * CakeKtaiLibrary for CakePHP1.3
+ * Copyright 2009-2012, ECWorks.
+ 
+ * Licensed under The GNU General Public Licence
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright		Copyright 2009-2012, ECWorks.
+ * @link			http://www.ecworks.jp/ ECWorks.
+ * @version			0.5.1
+ * @lastmodified	$Date: 2012-02-12 03:00:00 +0900 (Sun, 12 Feb 2012) $
+ * @license			http://www.gnu.org/licenses/gpl.html The GNU General Public Licence
+ */
+
 class CakeGroupTest extends TestSuite {
 	
 	var $label = 'Ktai Library codes for CakePHP';
--- views/helpers/ktai.php
+++ views/helpers/ktai.php
@@ -1,21 +1,21 @@
 <?php
-/**
- * Ktai library, supports Japanese mobile phone sites coding.
- * It provides many functions such as a carrier check to use Referer or E-mail, 
+/*
+ * CakeKtaiLibrary, supports Japanese mobile phone sites coding.
+ * This provides many functions such as a carrier check to use Referer or E-mail, 
  * conversion of an Emoji, and more.
  *
- * PHP versions 4 and 5
+ * PHP versions 5
  *
- * Ktai Library for CakePHP
- * Copyright 2009-2011, ECWorks.
+ * CakeKtaiLibrary for CakePHP1.3
+ * Copyright 2009-2012, ECWorks.
  
  * Licensed under The GNU General Public Licence
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright		Copyright 2009-2011, ECWorks.
+ * @copyright		Copyright 2009-2012, ECWorks.
  * @link			http://www.ecworks.jp/ ECWorks.
- * @version			0.4.1
- * @lastmodified	$Date: 2011-02-11 18:00:00 +0900 (Fri, 11 Feb 2011) $
+ * @version			0.5.1
+ * @lastmodified	$Date: 2012-02-12 03:00:00 +0900 (Sun, 12 Feb 2012) $
  * @license			http://www.gnu.org/licenses/gpl.html The GNU General Public Licence
  */
 
